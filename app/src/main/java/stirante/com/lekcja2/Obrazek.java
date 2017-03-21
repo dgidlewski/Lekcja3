62ef032fc7f389a5ffd4388d7f0d0b10180d56bc
dgidlewski <dariusz.gidlewski@gmail.com>
2017-03-21 11:24:49 +0100

    Initial commit

---
+++ app/src/main/java/stirante/com/lekcja2/Obrazek.java
@@ -0,0 +1,19 @@
+package stirante.com.lekcja2;
+
+/**
+ * Created by student on 2017-03-21.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Obrazek extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.obrazek);
+    }
+
+
+}
